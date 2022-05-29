c2263e19b563debf7a8f491675d07ade9f8557a2
Gustavo Mendes <guguren3@hotmail.com>
2022-05-29 09:19:03 -0300

    base

---
+++ src/FC.Codeflix.Catalog.Domain/Entity/Category.cs
@@ -0,0 +1,32 @@
+﻿using FC.Codeflix.Catalog.Domain.Exceptions;
+
+namespace FC.Codeflix.Catalog.Domain.Entity
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Category(string name, string description, bool isActive = true)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            IsActive = isActive;
+            CreatedAt = DateTime.Now;
+
+            Validate();
+        }
+
+        public void Validate()
+        {
+            if(string.IsNullOrWhiteSpace(Name))
+                throw new EntityValidationException($"{nameof(Name)} should not bem empty or null");
+            if (Description is null)
+                throw new EntityValidationException($"{nameof(Description)} should not bem empty or null");
+        }
+    }
+}
